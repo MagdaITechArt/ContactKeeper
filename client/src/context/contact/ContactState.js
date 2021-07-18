055f45720b84e14904d8f23f0f522c67fb2ee83f
Magdalena Oziębło <magdalena.ozieblo@itechart-group.com>
2021-07-18 22:56:05 +0200

    Contacts Context + global State + Contacts Component + ContactItems Component

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,59 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACT, CLEAR_FILTER} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: "Kevin Home",
+                email: "kevin@o2.pl",
+                phone: "111-111-111",
+                type: "personal"
+            },
+            {
+                id: 2,
+                name: "Sara White",
+                email: "sara@o2.pl",
+                phone: "333-111-111",
+                type: "professional"
+            },
+            {
+                id: 3,
+                name: "John Leg",
+                email: "john@o2.pl",
+                phone: "111-333-111",
+                type: "personal"
+            }
+        ]
+    };
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    return (
+        <ContactContext.Provider value={{
+            contacts: state.contacts
+        }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState;
